188632e045853dbc606dfb8789f51c6c1b68fbe1
Rich Fritsch-Gerdes <rfritsch-gerdes2@huskers.unl.edu>
2022-08-26 03:34:22 +0000

    added my info and major

--- hello.c
+++ hello.c
@@ -9,6 +9,8 @@ int main(int argc, char **argv) {
 
     printf("Matthew Haake\n");
     printf("Computer Science\n");
+    printf("Rich Fritsch-Gerdes\n");
+    printf("Computer Science\n");
 
     return 0;
 }
